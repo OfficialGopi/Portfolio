450ce8345508b50cbaa600ef6bb9448f6fa5f24a
Gopikanta Mondal <official.gopi@outlook.com>
2025-08-06 15:07:37 +0530

    Update projectLinks.js

--- src/constants/projectLinks.js
+++ src/constants/projectLinks.js
@@ -6,4 +6,10 @@ export const projects = [
     repoLink: "https://github.com/OfficialGopi/chatapp",
     demoLink: "https://chatty.officialgopi.xyz",
   },
+  {
+    imgUrl: `https://res.cloudinary.com/dko5qxqqi/image/upload/v1754472717/Screenshot_from_2025-08-06_14-59-59_l9dby0.png`,
+    title: "CoderRoute - An AI Powered Coding Platform",
+    description:"A LeetCode-inspired full-stack web application for solving coding problems, tracking daily streaks, and personalized revision. Built with MERN stack, TailwindCSS, and Framer Motion, it features a live code editor powered by Judge0 API for real-time code execution and an integrated AI assistant that provides hints, explanations, and debugging help. Includes full user auth, admin dashboard for problem management, and responsive design.", repoLink: "https://github.com/OfficialGopi/coderroute-main",
+    demoLink: "https://coderroute.officialgopi.xyz",
+  },
 ];
